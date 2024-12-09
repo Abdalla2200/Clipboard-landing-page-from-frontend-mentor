bd6ede112deb5010c3e1b52a5122bffc7f8a08d8
Abdalla2200 <abdallaessam3300@gmail.com>
2024-12-09 20:52:15 +0200

    Initial Upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Bai: ["Bai Jamjuree", "sans-serif"],
+      },
+      colors: {
+        StrongCyan: "hsl(171, 66%, 44%)",
+        LightBlue: "hsl(233, 100%, 69%)",
+        DarkGrayishBlue: "hsl(210, 10%, 33%)",
+        GrayishBlue: "hsl(201, 11%, 66%)",
+      },
+    },
+  },
+  plugins: [],
+};
